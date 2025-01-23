92a056ace07e3fabb54f01e645d7bd77d25e0780
Alexandr Dzyundzya <dzyundzya.aa@yandex.ru>
2025-01-23 10:33:21 +0300

    create kittybot

---
+++ kittybot.py
@@ -0,0 +1,68 @@
+import logging
+import os
+import requests
+
+from dotenv import load_dotenv
+from telebot import TeleBot, types
+
+
+load_dotenv()
+
+bot_token = os.getenv('TOKEN')
+bot = TeleBot(token=bot_token)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+URL = 'https://api.thecatapi.com/v1/images/search'
+
+
+def get_new_image():
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        print(error)
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+
+    response = response.json()
+    random_cat = response[0].get('url')
+    return random_cat
+
+
+@bot.message_handler(commands=['newcat'])
+def new_cat(message):
+    chat = message.chat
+    bot.send_photo(chat.id, get_new_image())
+
+
+@bot.message_handler(commands=['start'])
+def wake_up(message):
+    chat = message.chat
+    name = chat.first_name
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_newcat = types.KeyboardButton('/newcat')
+    keyboard.add(button_newcat)
+    bot.send_message(
+        chat_id=chat.id,
+        text=f'Привет, {name}. Посмотри, какого котика я тебе нашёл',
+        reply_markup=keyboard,
+        )
+    bot.send_photo(chat.id, get_new_image())
+
+
+@bot.message_handler(content_types=['text'])
+def say_hi(message):
+    chat = message.chat
+    chat_id = chat.id
+    bot.send_message(chat_id=chat_id, text='Привет, я Элис!')
+
+
+def main():
+    bot.polling()
+
+
+if __name__ == '__main__':
+    main()
